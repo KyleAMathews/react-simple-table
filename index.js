d901b613e9a320316c947a43d6e4975f14cc875e
Kyle Mathews <mathews.kyle@gmail.com>
2014-08-15 14:24:03 -0700

    New release

--- index.js
+++ index.js
@@ -19,8 +19,14 @@ for (var i = 0; i < 9; i++) {
   fakeBizData.push(newObject);
 }
 
+var fakeContactData = [];
+for (var i = 0; i < 9; i++) {
+  fakeContactData.push(faker.Helpers.createCard());
+}
+console.log(fakeContactData);
+
 React.renderComponent(
-  React.DOM.div({style:{margin:'0 auto', width: '400px'}},
+  React.DOM.div({style:{margin:'0 auto', width: '500px'}},
     [
       React.DOM.h1(null, "React-Simple-Table"),
       React.DOM.a({href:"https://github.com/KyleAMathews/react-simple-table"}, "Browse code on Github"),
@@ -42,5 +48,27 @@ React.renderComponent(
       React.DOM.br(),
       React.DOM.br(),
       SimpleTable({columns: ['Company', 'Sales', 'Revenue', 'Profit'], data: fakeBizData}),
+
+      React.DOM.br(),
+      React.DOM.br(),
+      React.DOM.hr(),
+      React.DOM.br(),
+      React.DOM.br(),
+      React.DOM.h2(null, "Access deep data using dot-notation"),
+      React.DOM.code(null, "<SimpleTable columns={['Name', 'Email', {displayName: 'Company', path: 'company.name'}]}, data={fakeContactData} />"),
+      React.DOM.br(),
+      React.DOM.br(),
+      SimpleTable({columns: ['Name', 'Email', {displayName: 'Company', path: 'company.name'}], data: fakeContactData}),
+
+      React.DOM.br(),
+      React.DOM.br(),
+      React.DOM.hr(),
+      React.DOM.br(),
+      React.DOM.br(),
+      React.DOM.h2(null, "Access or manipulate data using a function"),
+      React.DOM.code(null, "<SimpleTable columns={['Name', 'Email', {displayName: 'Company', function: function(data){return data.address.streetA + ', ' + data.address.city}}]}, data={fakeContactData} />"),
+      React.DOM.br(),
+      React.DOM.br(),
+      SimpleTable({columns: ['Name', 'Email', {displayName: 'Address', function: function(data){return data.address.streetA + ", " + data.address.city}}], data: fakeContactData}),
 
     ]), document.body);
