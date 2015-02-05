c2527e339ca7fab7062607b1e7c675d09bedc830
Kyle Mathews <mathews.kyle@gmail.com>
2015-02-04 21:01:26 -0800

    Fix example

--- examples/index.js
+++ examples/index.js
@@ -11,7 +11,7 @@ var fruitColors = [{
 var fakeBizData = [];
 for (var i = 0; i < 9; i++) {
   newObject = {
-    company: faker.Company.companyName(),
+    company: faker.company.companyName(),
     sales: faker.random.number(100,600),
     revenue: faker.random.number(1000,100000),
     profit: faker.random.number(1000,10000)
@@ -21,7 +21,7 @@ for (var i = 0; i < 9; i++) {
 
 var fakeContactData = [];
 for (var i = 0; i < 9; i++) {
-  fakeContactData.push(faker.Helpers.createCard());
+  fakeContactData.push(faker.helpers.createCard());
 }
 console.log(fakeContactData);
 
@@ -34,10 +34,10 @@ React.renderComponent(
       React.DOM.br(),
       React.DOM.br(),
 
-      React.DOM.code(null, "<SimpleTable columns={['apple', 'peach', 'cherry']}, data={fruitColors} />"),
+      React.DOM.code(null, "<SimpleTable className='test-class' columns={['apple', 'peach', 'cherry']}, data={fruitColors} />"),
       React.DOM.br(),
       React.DOM.br(),
-      SimpleTable({columns: ['Lime', 'Lemon', 'Orange'], data: fruitColors}),
+      SimpleTable({className: "test-class", columns: ['Lime', 'Lemon', 'Orange'], data: fruitColors}),
       React.DOM.br(),
       React.DOM.br(),
       React.DOM.hr(),
